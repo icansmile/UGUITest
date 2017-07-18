47c45477ce4be5519166030b9851695ab139d345
laishencan <laishencan@laishencandeMac-mini.local>
2017-07-18 18:27:56 +0800

    Sprite Packer & Atlas
    ing

---
+++ Assets/Sprite_Packer/Scripts/UGUISpritePacker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Resources文件夹下的资源将不会被打入图集
+//要先把图片的 [Texture Type] 设为 [Sprite]
+//接着指定 [Packing Tag]
+public class UGUISpritePacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
