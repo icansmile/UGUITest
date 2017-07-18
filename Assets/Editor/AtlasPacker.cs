5d1b33b9c04bcf4793fa65f3ac8f5757bc5bf326
Nick.Can <453558407@qq.com>
2017-07-18 23:52:30 +0800

    MakePrefabsWithAtlas
    TODO:
    了解PrefabUtility（PrefabUtility.CreatePrefab)
    了解AssetDatabase （AssetDatabase.LoadAssetAtPath）
    文件操作（获取具体Info, 才能使用Pattern搜索文件）

---
+++ Assets/Editor/AtlasPacker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+public class AtlasPacker {
+
+	[MenuItem("MyTool/MakePrefabsWithAtlas")]
+	private static void MakePrefabsWithAtlas()
+	{
+		string prefabDir = Application.dataPath + "/Resources/Sprite";
+
+		if(!Directory.Exists(prefabDir))
+		{
+			Directory.CreateDirectory(prefabDir);
+		}
+
+		//获取Atlas目录下所有的png, 创建对应的sprite prefab， 存进 prefabDir
+		string atlasPicDir = Application.dataPath + "/Sprite_Packer/Atlas";
+
+		//获取DirectionInfo才能使用pattern搜索文件名
+		DirectoryInfo atlasPicDirInfo = new DirectoryInfo(atlasPicDir);
+
+		foreach(DirectoryInfo dirInfo in atlasPicDirInfo.GetDirectories())
+		{
+			// Debug.Log(dirInfo.FullName);
+			foreach(FileInfo fileInfo in dirInfo.GetFiles("*.png"))
+			{
+				string picPath = fileInfo.FullName;
+				string assetPath = picPath.Substring(picPath.IndexOf("Assets"));
+				// Debug.Log(assetPath);
+
+				Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(assetPath);
+				GameObject spriteGo = new GameObject(sprite.name);
+				SpriteRenderer spriteRenderer = spriteGo.AddComponent<SpriteRenderer>();
+				spriteRenderer.sprite = sprite;
+
+				string prefabPath = prefabDir + "/" + sprite.name + ".prefab";
+				PrefabUtility.CreatePrefab(prefabPath.Substring(prefabPath.IndexOf("Assets")), spriteGo);
+
+				//Destroy may not be called from edit mode! Use DestroyImmediate instead.
+				GameObject.DestroyImmediate(spriteGo);
+			}
+		}
+
+		AssetDatabase.Refresh();
+
+	}
+}
