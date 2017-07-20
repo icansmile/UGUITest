366e78c2e7d59ee6962957b7a725fbd2a1aa5789
Nick.Can <453558407@qq.com>
2017-07-20 23:57:27 +0800

    Editor & Handles & OnInspectorGUI & OnSceneGUI

--- Assets/Editor_Learn/Editor/Editor_Leran.cs
+++ Assets/Editor_Learn/Editor/Editor_Leran.cs
@@ -4,11 +4,49 @@ using UnityEngine;
 using UnityEditor;
 
 /// <summary>
+/// Editor只要是负责显示 附加在Inspector检视窗中的UI  https://docs.unity3d.com/Manual/editor-CustomEditors.html
+/// Handles只要是用来描绘Scene窗口中的GUI， 例如坐标轴等 https://docs.unity3d.com/ScriptReference/Handles.html
+/// 
+/// 
 /// 参考:
 /// 
-/// https://code.tutsplus.com/zh-hans/tutorials/how-to-add-your-own-tools-to-unitys-editor--active-10047
+/// 有空看[如何在Unity编辑器中添加你自己的工具,绘制你自己的gizmo] https://code.tutsplus.com/zh-hans/tutorials/how-to-add-your-own-tools-to-unitys-editor--active-10047
 /// http://blog.csdn.net/jjiss318/article/details/7435708
 /// </summary>
+
+[CustomEditor(typeof(LookAtPoint))]
+[CanEditMultipleObjects]
 public class Editor_Leran : Editor
 {
+    SerializedProperty lookAtPoint;
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    {
+        lookAtPoint = serializedObject.FindProperty("lookAtPoint");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(lookAtPoint);
+        EditorGUILayout.LabelField("这里是LabelField,可以和在OnGUI一样使用各种描绘方法");
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public void OnSceneGUI()
+    {
+        var t = (target as LookAtPoint);
+
+        EditorGUI.BeginChangeCheck();
+        Vector3 pos = Handles.PositionHandle(t.transform.position, Quaternion.identity);
+        if(EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Move Point");
+            t.lookAtPoint = pos;
+            t.Update();
+        }
+    }
 }
---
+++ Assets/Editor_Learn/Scripts/LookAtPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+[CanEditMultipleObjects]
+public class LookAtPoint : MonoBehaviour {
+
+	public Vector3 lookAtPoint = Vector3.zero;
+
+	// Update is called once per frame
+	public void Update () {
+		transform.LookAt(lookAtPoint);
+	}
+}
