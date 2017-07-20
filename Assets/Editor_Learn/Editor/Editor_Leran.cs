7735ad6a59f8e91977d1b0eb8bd09f5cdd65de07
laishencan <laishencan@laishencandeMac-mini.local>
2017-07-20 18:20:52 +0800

    Editor

---
+++ Assets/Editor_Learn/Editor/Editor_Leran.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 参考:
+/// 
+/// https://code.tutsplus.com/zh-hans/tutorials/how-to-add-your-own-tools-to-unitys-editor--active-10047
+/// http://blog.csdn.net/jjiss318/article/details/7435708
+/// </summary>
+public class Editor_Leran : Editor
+{
+}
