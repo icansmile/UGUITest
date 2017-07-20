fc83a9d910957ebda08bb85da457d411625e0f29
laishencan <laishencan@laishencandeMac-mini.local>
2017-07-20 18:12:43 +0800

    EditorWindow
    有几个要思考的地方需要总结下

---
+++ Assets/Editor_Learn/Editor/EditorWindow_Learn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+/// <summary>
+/// 
+/// 参考
+/// 官方API文档 https://docs.unity3d.com/ScriptReference/EditorWindow.html
+/// </summary>
+public class EditorWindow_Learn : EditorWindow
+{
+	string myString = "Hello World";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    [MenuItem("Editor Tool/EditorWindow")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        EditorWindow_Learn window = (EditorWindow_Learn)EditorWindow.GetWindow(typeof(EditorWindow_Learn));
+		window.titleContent = new GUIContent("这里是标题");
+        window.Show();
+		window.ShowNotification((new GUIContent("提示你一下")));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        myString = EditorGUILayout.TextField("Text Field", myString);
+
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+        myBool = EditorGUILayout.Toggle("Toggle", myBool);
+        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+        EditorGUILayout.EndToggleGroup();
+    }
+}
