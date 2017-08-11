66d2ab8a0448b3c80a5c3da0fbba16433c9c7f1a
laishencan <laishencan@laishenandeMini.lan>
2017-08-11 16:47:27 +0800

    socket & CSharpProjects
    超简陋socket实现 - 客户端+服务端
    生成dll

---
+++ Assets/NetWork_Learn/Scripts/TestSocket.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;  
+using System.Collections;  
+using Net;  
+using System.Threading;
+  
+public class TestSocket : MonoBehaviour {  
+    private Thread thread;
+    private ClientSocket clientSocket;
+  
+    // Use this for initialization  
+    void Start () {  
+        
+        // ThreadPool.QueueUserWorkItem(obj => {
+        //     ClientSocket mSocket = new ClientSocket();  
+        //     mSocket.ConnectServer("127.0.0.1", 8088);  
+        //     mSocket.SendMessage("服务器傻逼！");  
+        // });
+        thread = new Thread(() => {
+            clientSocket = new ClientSocket();  
+            clientSocket.ConnectServer("127.0.0.1", 8088);  
+            clientSocket.SendMessage("服务器傻逼！");  
+        });
+        thread.Start();
+    }  
+      
+    // Update is called once per frame  
+    void Update () {  
+      
+    }  
+
+    /// <summary>
+    /// OnGUI is called for rendering and handling GUI events.
+    /// This function can be called multiple times per frame (one call per event).
+    /// </summary>
+    void OnGUI()
+    {
+        if(GUILayout.Button("发送"))
+        {
+            clientSocket.SendMessage("send test");
+        }
+    }
+
+    /// <summary>
+    /// This function is called when the behaviour becomes disabled or inactive.
+    /// </summary>
+    void OnDisable()
+    {
+        thread.Abort();
+    }
+}  
---
+++ CSharpProjects/dllTest/dllTest/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dllTest
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+
+		public string testA()
+		{
+			return "test A";
+		}
+
+		private string testB()
+		{
+			return "test B";
+		}
+	}
+}
+
